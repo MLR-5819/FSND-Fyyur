f2147794ff5fd9bf3d6d62d6fd9633754556cbd8
MLR-5819 <58010528+MLR-5819@users.noreply.github.com>
2020-07-10 18:09:53 -0500

    updated

--- flask_app/app.py
+++ flask_app/app.py
@@ -532,7 +532,7 @@ def shows():
 
     data.append({
       "venue_id": show.venue_id,
-      "venue_name": show.venue.name ,
+      "venue_name": show.venue.name,
       "artist_id": show.artist_id,
       "artist_name": show.artist.name,
       "artist_image_link": show.artist.image_link,
--- flask_app/config.py
+++ flask_app/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
-# TODO IMPLEMENT DATABASE URL
+# DONE IMPLEMENT DATABASE URL
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ENV = 'development'
